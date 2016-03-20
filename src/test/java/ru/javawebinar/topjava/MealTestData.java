a294c5e9723e856d91d4193e4e4fa3fa5be3c9b2
Alk <baldessarinii@mail.ru>
2016-03-21 00:02:23 +0300

    add test data UserMeal

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -1,13 +1,36 @@
 package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.matcher.ModelMatcher;
+import ru.javawebinar.topjava.model.BaseEntity;
 import ru.javawebinar.topjava.model.UserMeal;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 /**
  * GKislin
  * 13.03.2015.
  */
 public class MealTestData {
+
+    public static final int MEAL1_ID = BaseEntity.START_SEQ + 2;
+
+    public static final UserMeal MEAL1 = new UserMeal(MEAL1_ID, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+    public static final UserMeal MEAL2 = new UserMeal(MEAL1_ID + 1, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000);
+    public static final UserMeal MEAL3 = new UserMeal(MEAL1_ID + 2, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500);
+    public static final UserMeal MEAL4 = new UserMeal(MEAL1_ID + 3, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000);
+    public static final UserMeal MEAL5 = new UserMeal(MEAL1_ID + 4, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500);
+    public static final UserMeal MEAL6 = new UserMeal(MEAL1_ID + 5, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510);
+
+    public static UserMeal getSaved() {
+        return new UserMeal(100008,LocalDateTime.of(2015, Month.MAY, 31, 21, 0), "saved", 300);
+    }
+//
+    public static UserMeal getUpdated() {
+        UserMeal updated = MEAL1;
+        updated.setDescription("Update");
+        return updated;
+    }
 
     public static final ModelMatcher<UserMeal, String> MATCHER = new ModelMatcher<>(UserMeal::toString);
 
