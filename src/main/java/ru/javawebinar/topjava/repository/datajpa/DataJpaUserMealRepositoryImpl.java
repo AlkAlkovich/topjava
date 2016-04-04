c6990a163b4974e54fa1f3e5adb07e569fd509be
Alk <baldessarinii@mail.ru>
2016-04-04 23:17:24 +0300

    add profiles: jdbc,jpa,dataJpa

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 
@@ -12,29 +14,59 @@ import java.util.List;
  * 27.03.2015.
  */
 @Repository
-public class DataJpaUserMealRepositoryImpl implements UserMealRepository{
+@Transactional(readOnly = true)
+public class DataJpaUserMealRepositoryImpl implements UserMealRepository {
+
+    @Autowired
+    private ProxyUserMealRepository proxyUserMealRepository;
+    @Autowired
+    private ProxyUserRepository proxyUserRepository;
+
     @Override
+    @Transactional
     public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+
+        if (userMeal.isNew()) {
+            userMeal.setUser(proxyUserRepository.getOne(userId));
+            return proxyUserMealRepository.save(userMeal);
+        } else {
+            return get(userMeal.getId(), userId) == null
+                    ? null
+                    : proxyUserMealRepository.save(userMeal);
+        }
+
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        if(get(id,userId).getUser().getId()==userId){
+            proxyUserMealRepository.delete(id);
+            return true;
+        }else {
+            return false;
+        }
+
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        UserMeal userMeal = proxyUserMealRepository.findOne(id);
+        if (userMeal.getUser().getId() == userId) {
+            return proxyUserMealRepository.findOne(id);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<UserMeal> getAll(int userId) {
-        return null;
+        return proxyUserMealRepository.findByUserIdOrderByDateTimeDesc(userId);
     }
 
     @Override
+    @Transactional
     public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return proxyUserMealRepository.findByUserIdAndDateTimeBetweenOrderByDateTimeDesc(startDate, endDate, userId);
     }
 }
