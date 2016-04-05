f410e049030f8936e1a5991a73ffc895245ceeaf
Alk <baldessarinii@mail.ru>
2016-04-05 23:24:13 +0300

    add profiles to SpringMain and MealServlet

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
 import ru.javawebinar.topjava.web.meal.UserMealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
@@ -19,13 +19,20 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/mock.xml")) {
-            System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+
+
+//        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/mock.xml")) {
+        try (GenericXmlApplicationContext ctx = new GenericXmlApplicationContext()) {
+            ConfigurableEnvironment env = ctx.getEnvironment();
+            env.setActiveProfiles(Profiles.POSTGRES);
+            ctx.load("spring/spring-app.xml", "spring/mock.xml");
+            ctx.refresh();
+            System.out.println(Arrays.toString(ctx.getBeanDefinitionNames()));
+            AdminRestController adminUserController = ctx.getBean(AdminRestController.class);
             System.out.println(adminUserController.create(UserTestData.USER));
             System.out.println();
 
-            UserMealRestController mealController = appCtx.getBean(UserMealRestController.class);
+            UserMealRestController mealController = ctx.getBean(UserMealRestController.class);
             List<UserMealWithExceed> filteredMealsWithExceeded =
                     mealController.getBetween(
                             LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
